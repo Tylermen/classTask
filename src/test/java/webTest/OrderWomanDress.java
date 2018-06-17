a0f8db61877f808ca90f8807a22adc13016e00aa
Tylerman <ikirillovs@gmail.com>
2018-06-17 18:03:22 +0300

    version 1

--- src/test/java/webTest/OrderWomanDress.java
+++ src/test/java/webTest/OrderWomanDress.java
@@ -1,5 +1,6 @@
 package webTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +10,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class OrderWomanDress {
 private final static String HOME_PAGE = "http://automationpractice.com/";
 private final By WOMEN = By.xpath("//a[@title='Women']");
+private final By DRESSES = By.xpath("//ul[@id='ul_layered_category_0']/li[2]/label/a");
+private final By ORANGE = By.xpath("//ul[@id='ul_layered_category_0']/li[2]/label/a");
 
 
 @Test
@@ -21,6 +24,12 @@ public void orederFromWeb () {
 
     WebElement women = driver.findElement(WOMEN);
     women.click();
+
+    WebElement dresses = driver.findElement(DRESSES);
+    dresses.click();
+
+    Assert.assertEquals(
+            "", driver.getTitle());
 
 
     driver.quit();
